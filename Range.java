4c9530abedd4d292f4cf7d853e6562aa355dea13
Rahulmanda77 <rahulmanda777@gmail.com>
2026-09-02 16:23:55 +0530

    Delete Range.java

--- Range.java
+++
@@ -1,9 +0,0 @@
-class Range{
-public static void main(String[] args)
-{
-System.out.println("Byte Range: " + Byte.MIN_VALUE + "to" + Byte.MAX_VALUE);
-System.out.println("Short Range: " + Short.MIN_VALUE + "to" + Short.MAX_VALUE);
-System.out.println("Int Range: " + Integer.MIN_VALUE + "to" + Integer.MAX_VALUE);
-System.out.println("Long Range: " + Long.MIN_VALUE + "to" + Long.MAX_VALUE);
-}
-}
