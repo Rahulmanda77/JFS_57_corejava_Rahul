16b5bf2fcddd9f78836da191b266c9fc7454fa46
Rahulmanda77 <rahulmanda777@gmail.com>
2026-09-02 16:22:50 +0530

    Delete Bio.java

--- Bio.java
+++
@@ -1,9 +0,0 @@
-class Bio
-{
-public static void main(String[] args){
-System.out.println("Name:Rahul");
-System.out.println("Age:21");
-System.out.println("Gender:Male");
-System.out.println("City:Mancherial");
-}
-}
